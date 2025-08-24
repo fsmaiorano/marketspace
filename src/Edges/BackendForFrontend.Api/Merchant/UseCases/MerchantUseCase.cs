13699202029680ac53e0c8731500a7ecd797723e
Fábio <fsmaiorano@gmail.com>
2025-08-24 11:14:18 +0100

    Add logging for merchant operations; enhance traceability in Create, Update, Delete, and Get methods

--- src/Edges/BackendForFrontend.Api/Merchant/UseCases/MerchantUseCase.cs
+++ src/Edges/BackendForFrontend.Api/Merchant/UseCases/MerchantUseCase.cs
@@ -8,21 +8,25 @@ public class MerchantUseCase(ILogger<MerchantUseCase> logger, IMerchantService s
 {
     public async Task<CreateMerchantResponse> CreateMerchantAsync(CreateMerchantRequest request)
     {
+        logger.LogInformation("Creating merchant with name: {MerchantName}", request.Name);
         return await service.CreateMerchantAsync(request);
     }
 
     public async Task<UpdateMerchantResponse> UpdateMerchantAsync(UpdateMerchantRequest request)
     {
+        logger.LogInformation("Updating merchant with ID: {MerchantId}", request.MerchantId);
         return await service.UpdateMerchantAsync(request);
     }
 
     public async Task<DeleteMerchantResponse> DeleteMerchantAsync(Guid merchantId)
     {
+        logger.LogInformation("Deleting merchant with ID: {MerchantId}", merchantId);
         return await service.DeleteMerchantAsync(merchantId);
     }
 
     public async Task<GetMerchantByIdResponse> GetMerchantByIdAsync(Guid merchantId)
     {
+        logger.LogInformation("Retrieving merchant with ID: {MerchantId}", merchantId);
         return await service.GetMerchantByIdAsync(merchantId);
     }
 }
