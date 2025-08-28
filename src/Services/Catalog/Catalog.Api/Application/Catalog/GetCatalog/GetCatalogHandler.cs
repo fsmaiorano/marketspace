9637a2f628333ae4abd6b5396927ece96e4a4dd5
Fábio <fsmaiorano@gmail.com>
2025-08-28 22:01:52 +0100

    feat: Integrate Minio storage for catalog image retrieval and update pagination defaults

--- src/Services/Catalog/Catalog.Api/Application/Catalog/GetCatalog/GetCatalogHandler.cs
+++ src/Services/Catalog/Catalog.Api/Application/Catalog/GetCatalog/GetCatalogHandler.cs
@@ -1,12 +1,16 @@
 using BuildingBlocks;
 using BuildingBlocks.Pagination;
+using BuildingBlocks.Storage.Minio;
 using Catalog.Api.Application.Dtos;
 using Catalog.Api.Domain.Entities;
 using Catalog.Api.Domain.Repositories;
 
 namespace Catalog.Api.Application.Catalog.GetCatalog;
 
-public class GetCatalogHandler(ICatalogRepository repository, ILogger<GetCatalogHandler> logger) : IGetCatalogHandler
+public class GetCatalogHandler(
+    ICatalogRepository repository,
+    ILogger<GetCatalogHandler> logger,
+    IMinioBucket minioBucket) : IGetCatalogHandler
 {
     public async Task<Result<GetCatalogResult>> HandleAsync(GetCatalogQuery query)
     {
@@ -14,18 +18,27 @@ public class GetCatalogHandler(ICatalogRepository repository, ILogger<GetCatalog
 
         PaginatedResult<CatalogEntity> products = await repository.GetPaginatedListAsync(query.Pagination);
 
-        List<CatalogDto> catalogDtoList = products.Data.Select(product => new CatalogDto
+        List<CatalogDto> catalogDtoList = [];
+        foreach (CatalogEntity product in products.Data)
         {
-            Id = product.Id.Value,
-            Name = product.Name,
-            Categories = product.Categories,
-            Description = product.Description,
-            ImageUrl = product.ImageUrl,
-            Price = product.Price.Value,
-            MerchantId = product.MerchantId,
-            CreatedAt = product.CreatedAt,
-            UpdatedAt = product.UpdatedAt
-        }).ToList();
+            CatalogDto catalogDto = new CatalogDto
+            {
+                Id = product.Id.Value,
+                Name = product.Name,
+                Categories = product.Categories,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                Price = product.Price.Value,
+                MerchantId = product.MerchantId,
+                CreatedAt = product.CreatedAt,
+                UpdatedAt = product.UpdatedAt
+            };
+
+            string imageData = await minioBucket.GetImageAsync(product.ImageUrl) ?? string.Empty;
+            catalogDto.ImageUrl = imageData;
+
+            catalogDtoList.Add(catalogDto);
+        }
 
         return Result<GetCatalogResult>.Success(
             new GetCatalogResult(products.PageIndex, products.PageSize, products.Count, catalogDtoList.ToList()));
