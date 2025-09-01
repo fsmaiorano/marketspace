fd29200e0abac6013b59e2051e3d2bd69aa069b8
Fábio Maiorano <fsmaiorano@gmail.com>
2025-09-01 14:44:09 +0100

    feat: Implement GetBasketResponse DTO and update MarketSpaceService for basket management

--- src/Uis/WebApp/Controllers/CartController.cs
+++ src/Uis/WebApp/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Dtos;
 using WebApp.Services;
 
 namespace WebApp.Controllers;
@@ -10,9 +11,14 @@ public class CartController(IMarketSpaceService service, ILogger<CartController>
     public IActionResult CartHandler(string productId)
     {
         logger.LogInformation("Adding product {ProductId} to cart", productId);
-        
+
+        CreateOrUpdateBasketRequest createBasketRequest = new()
+        {
+            Username = "mock", Items = []
+        };
+
         var response = new { Success = true, Message = "Product added to cart", ProductId = productId };
-        
+
         return Json(response);
     }
 }
