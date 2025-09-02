789dbe832b773da03d072d72efdd9f58baaca1a3
Fábio Maiorano <fsmaiorano@gmail.com>
2025-09-02 14:37:37 +0100

    feat: Refactor basket retrieval to return ShoppingCartDto and handle null cases gracefully

--- src/Uis/WebApp/Dtos/GetBasketResponse.cs
+++ src/Uis/WebApp/Dtos/GetBasketResponse.cs
@@ -2,6 +2,5 @@ namespace WebApp.Dtos;
 
 public class GetBasketResponse
 {
-    public string Username { get; set; } = string.Empty;
-    public List<ShoppingCartItemDto> Items { get; set; } = [];
+    public ShoppingCartDto Cart { get; set; } = new();
 }
