0262378cf994800e52fe44725f17327b5bbca51b
Fábio <fsmaiorano@gmail.com>
2025-09-02 15:10:37 +0100

    feat: Simplify GetBasketByIdResult constructor and add TotalPrice property to ShoppingCartDto

--- src/Uis/WebApp/Dtos/ShoppingCartDto.cs
+++ src/Uis/WebApp/Dtos/ShoppingCartDto.cs
@@ -4,4 +4,5 @@ public class ShoppingCartDto
 {
     public string Username { get; set; } = string.Empty;
     public List<ShoppingCartItemDto> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
 }
