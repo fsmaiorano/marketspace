a3c5d67895c29223a20ec653ba50c24d18ad06a1
Fábio Maiorano <fsmaiorano@gmail.com>
2025-09-04 12:54:14 +0100

    Refactor code structure for improved readability and maintainability

--- src/Uis/WebApp/Dtos/GetBasketResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApp.Dtos;
-
-public class GetBasketResponse
-{
-    public ShoppingCartDto ShoppingCart { get; set; }
-}
--- src/Uis/WebApp/Dtos/ShoppingCartItemDto.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApp.Dtos;
-
-public class ShoppingCartItemDto
-{
-    public int Quantity { get; set; }
-    public decimal Price { get; set; }
-    public Guid ProductId { get; set; }
-    public string ProductName { get; set; } = string.Empty;
-}
