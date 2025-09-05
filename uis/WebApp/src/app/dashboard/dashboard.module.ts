a44db5cbd94335cd4f12203fd8c04d98098a9c43
Fábio <fsmaiorano@gmail.com>
2025-09-05 09:09:17 +0100

    feat: Implement CardComponent with template, styles, and tests; update dashboard to use CardComponent

--- uis/WebApp/src/app/dashboard/dashboard.module.ts
+++ uis/WebApp/src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { SharedModule } from '@app/shared/shared.module';
 
 @NgModule({
   exports: [DashboardComponent],
-  imports: [CommonModule, DashboardComponent],
+  imports: [CommonModule, SharedModule, DashboardComponent],
 })
 export class DashboardModule {}
