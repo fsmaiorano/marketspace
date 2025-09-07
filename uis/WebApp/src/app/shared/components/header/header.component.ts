b46b3fea380736be7f773dd5589dcdfb18173d6f
Fábio <fsmaiorano@gmail.com>
2025-09-07 10:23:14 +0100

    feat: Implement HeaderComponent with HTML structure, SCSS styling, and Angular Material integration

--- uis/WebApp/src/app/shared/components/header/header.component.ts
+++ uis/WebApp/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
