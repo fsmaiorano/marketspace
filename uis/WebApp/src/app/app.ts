c40a0d8320e49af30a4f59d7902c093b0ec3448c
Fábio <fsmaiorano@gmail.com>
2025-09-08 17:17:17 +0100

    feat: Add CheckoutComponent and update routing for checkout functionality

--- uis/WebApp/src/app/app.ts
+++ uis/WebApp/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardModule } from './dashboard/dashboard.module';
 import { HeaderComponent } from './shared/components/header/header.component';
+import {DashboardModule} from '@app/features/dashboard/dashboard.module';
+import {CheckoutModule} from '@app/features/checkout/checkout.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardModule, HeaderComponent],
+  imports: [RouterOutlet, DashboardModule, CheckoutModule, HeaderComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
   standalone: true,
