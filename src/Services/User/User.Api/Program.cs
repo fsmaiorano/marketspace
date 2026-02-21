c6a996283718c55a2850c289ccf6b8bb645748ad
Fábio Maiorano <fsmaiorano@gmail.com>
2026-02-21 13:21:09 -0300

    refactor: modularize application and infrastructure service registration, move database initialization logic to infrastructure layer, remove in-memory db config

--- src/Services/User/User.Api/Program.cs
+++ src/Services/User/User.Api/Program.cs
@@ -1,101 +1,17 @@
 using MarketSpace.ServiceDefaults;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi;
-using User.Api.Data;
-using User.Api.Data.Models;
-using User.Api.Endpoints;
+using User.Api.Application;
+using User.Api.Infrastructure;
 using User.Api.Extensions;
-using User.Api.Services;
-using BuildingBlocks.Messaging.Idempotency;
+using User.Api.Endpoints;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-
-bool useInMemoryDatabase = builder.Configuration.GetValue<bool>("UseInMemoryDatabase") ||
-                           builder.Environment.IsEnvironment("Testing");
-
-bool isUnitTest = AppDomain.CurrentDomain.GetAssemblies().Any(a =>
-    a.FullName != null && (a.FullName.Contains("xunit") || a.FullName.Contains("nunit") ||
-                           a.FullName.Contains("testhost")));
-
-builder.Services.AddDbContext<UserDbContext>(options =>
-{
-    if (useInMemoryDatabase || isUnitTest)
-        options.UseInMemoryDatabase("UserDb");
-    else
-        options.UseNpgsql(builder.Configuration.GetConnectionString("userdb"));
-});
-
-builder.Services.AddIdempotency<UserDbContext>();
-
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-    {
-        options.User.RequireUniqueEmail = true;
-        options.Password.RequiredLength = 8;
-        options.Password.RequireDigit = false;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireNonAlphanumeric = false;
-    })
-    .AddEntityFrameworkStores<UserDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
-JwtSettings jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>()!;
-
-byte[] key = Encoding.UTF8.GetBytes(jwtSettings.Key);
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = false; // Set to true in production
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidateAudience = true,
-            ValidAudience = jwtSettings.Audience,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.FromSeconds(30)
-        };
-    });
-
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-    });
-});
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "User.Api API",
-        Version = "v1",
-    });
-});
+// register application & infrastructure services
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration);
 
 WebApplication app = builder.Build();
 
@@ -106,14 +22,11 @@ app.UseSwaggerUI(options =>
     options.RoutePrefix = string.Empty;
 });
 
-// if (app.Environment.IsDevelopment())
-// {
-//     using var scope = app.Services.CreateScope();
-//     var dbContext = scope.ServiceProvider.GetRequiredService<UserDbContext>();
-//     dbContext.Database.Migrate();
-// }
-
-await app.InitialiseDatabaseAsync();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    await app.InitialiseDatabaseAsync();
+}
 
 app.UseCors();
 
@@ -127,5 +40,5 @@ app.Run();
 
 namespace User.Api.Api
 {
-    public partial class UserProgram;
+    public partial class UserProgram { }
 }
